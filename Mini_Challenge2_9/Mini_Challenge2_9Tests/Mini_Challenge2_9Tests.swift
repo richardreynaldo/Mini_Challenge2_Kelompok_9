0da35b99bdd32ae1a9f3ee53bf2db0fe0e0d204e
richardreynaldo <richardreynaldo626@gmail.com>
2020-05-06 21:53:10 +0700

    Base Code

---
+++ Mini_Challenge2_9/Mini_Challenge2_9Tests/Mini_Challenge2_9Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Mini_Challenge2_9Tests.swift
+//  Mini_Challenge2_9Tests
+//
+//  Created by Laurentius Richard Reynaldo on 06/05/20.
+//  Copyright © 2020 Laurentius Richard Reynaldo. All rights reserved.
+//
+
+import XCTest
+@testable import Mini_Challenge2_9
+
+class Mini_Challenge2_9Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
